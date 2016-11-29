022573733d30e0190d64bea6515b8231d54f720e
Minhan Xia <mixia@google.com>
2016-11-28 17:40:03 -0800

    add pushkubeproxy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kubescript
+
+A bunch of utility scripts to facilitate development/testing/debugging for k8s.
--- util.py
+++ util.py
@@ -1,10 +1,18 @@
 import subprocess
+import os
+import shutil
+import datetime
 from pprint import pprint
 
 GET_ALL_PODS = ["kubectl", "get", "pods", "--all-namespaces", "-o", "wide", "--show-all"]
 GET_ALL_GCE_INSTANCES = ["gcloud", "compute", "instances", "list"]
 GCLOUD_SSH = ["gcloud", "compute", "ssh", "--zone"]
 GCLOUD_COPY_FILE = ["gcloud", "compute", "copy-files"]
+KUBE_BASE_IMAGE = "gcr.io/google_containers/debian-iptables-amd64:v4"
+TMP_DOCKER_BUILD_PATH = "~/Desktop/tmp/mydockerbuild/"
+DOCKERFILE = "Dockerfile"
+TMP_DOCKER_FILE_PATH = os.path.join(TMP_DOCKER_BUILD_PATH, DOCKERFILE)
+
 
 class Pod:
     def __init__(self):
@@ -17,6 +25,7 @@ class Pod:
         self.node = node
         self.status = status
         pass
+
 
 class GCEInstance:
     def __init__(self):
@@ -33,6 +42,7 @@ class GCEInstance:
     def __str__(self):
         return self.name + '\t' + self.zone + '\t' + self.internal_ip + '\t' + self.external_ip + '\t' + self.status
 
+
 def get_all_pods():
     output = subprocess.check_output(GET_ALL_PODS)
     pods = []
@@ -41,19 +51,23 @@ def get_all_pods():
         pods.append(Pod(cols[0], cols[1], cols[6], cols[7], cols[3]))
     return pods
 
+
 def find_pod_with_prefix(pods, prefix):
     for pod in pods:
         if pod.name.startswith(prefix):
             return pod
     return None
 
+
 def find_all_pods_with_prefix(pods, prefix):
     ret = [p for p in pods if p.name.startswith(prefix)]
     return ret
 
+
 def exec_in_pod(pod, commands):
     print("##### Exec in %s/%s" % (pod.namespace, pod.name))
     exec_cmd(["kubectl", "--namespace", pod.namespace, "exec", "-it", pod.name] + commands)
+
 
 def get_all_gce_instances():
     output = subprocess.check_output(GET_ALL_GCE_INSTANCES)
@@ -68,15 +82,53 @@ def get_all_gce_instances():
         instances.append(GCEInstance(cols[0], cols[1], cols[3], cols[4], cols[5], ))
     return instances
 
+
 def gcloud_compute_ssh(zone, host):
     exec_cmd(GCLOUD_SSH + [zone, host])
+
 
 def gcloud_compute_ssh_command(zone, host, cmd):
     exec_cmd(GCLOUD_SSH + [zone, host] + ["--command", cmd])
 
+
 def gcloud_compute_copy_file_to_host(host, file):
-    exec_cmd(GCLOUD_COPY_FILE + [file, host+":"])
+    exec_cmd(GCLOUD_COPY_FILE + [file, host + ":"])
+
 
 def exec_cmd(cmd):
     print "+ " + ' '.join(cmd)
     subprocess.call(cmd)
+
+
+def build_kube_image(baseimage, binary_path, binary_name, tag="mykubetag"):
+    if os.path.exists(os.path.dirname(TMP_DOCKER_BUILD_PATH)):
+        shutil.rmtree(TMP_DOCKER_BUILD_PATH)
+    os.makedirs(os.path.dirname(TMP_DOCKER_BUILD_PATH))
+
+    simlink_path = os.path.join(TMP_DOCKER_BUILD_PATH, binary_name)
+    exec_cmd(["ln", binary_path, simlink_path])
+
+    f = open(TMP_DOCKER_FILE_PATH, 'w+')
+    f.write("FROM " + baseimage + "\n")
+    f.write("ADD " + binary_name + " /usr/local/bin/" + binary_name + "\n")
+    f.close()
+    exec_cmd(["docker", "build", "-t", tag, TMP_DOCKER_BUILD_PATH])
+
+
+def docker_rmi(tag):
+    exec_cmd(["docker", "rmi", tag])
+
+
+def build_kube_image_tarball(tag, output_path="/tmp/mydockerfile/out.tar"):
+    f = open(output_path, 'w+')
+    subprocess.call(["docker", "save", tag], stdout=f)
+    f.close()
+
+def search_binary_in_k8s_output_path(binary):
+    gopath = os.environ['GOPATH']
+    bin_path = os.path.join(gopath, 'src/k8s.io/kubernetes/_output/bin', binary)
+    if not os.path.isfile(bin_path):
+        raise Exception("Cannot find %s binary in %s" % (binary, bin_path))
+    print "================Found %s Binary================" % binary
+    exec_cmd(["ls", "-al", bin_path])
+    return bin_path
